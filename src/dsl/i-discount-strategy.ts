883b16b39794e00d8d8c746d9eab5333b635324c
Vladimir Rondelli <vladimir.rondeli@mutantbr.com>
2019-10-30 13:22:36 -0300

    Change iDiscountStrategy to abstract class

--- src/dsl/i-discount-strategy.ts
+++ src/dsl/i-discount-strategy.ts
@@ -1,7 +1,16 @@
 import { IDiscountRule } from './i-discount-rule';
-import { Product } from '../domain/entities/product';
-import { Discount } from '../domain';
+import { Discount, Product } from '../domain';
 
-export interface IDiscountStrategy {
-  execute(discountRules: IDiscountRule[], product: Product): Discount;
+export abstract class IDiscountStrategy {
+  abstract execute(discountRules: IDiscountRule[], product: Product): Discount;
+
+  protected applyDiscount(percentageValue: number, product: Product): Discount {
+    const percentageDivisor = 100;
+
+    const discountMultiplier = percentageValue / percentageDivisor;
+
+    const valueInCents = product.priceInCents * discountMultiplier;
+
+    return new Discount(parseFloat(percentageValue.toString()), valueInCents);
+  }
 }
