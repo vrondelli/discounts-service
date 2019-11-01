8055ce9be1daa07183d35cb4416523ec5ae1d202
Vladimir Rondelli <vladimir.rondeli@mutantbr.com>
2019-11-01 10:55:03 -0300

    Add discount rules and strategies name constants

---
+++ src/constants/discount-rules-names.ts
@@ -0,0 +1,3 @@
+export const BlackFridayDiscountRuleName = Symbol.for('BlackFridayRule');
+
+export const BirthdayDiscountRuleName = Symbol.for('BirthdayRule');
---
+++ src/constants/discount-strategies-names.ts
@@ -0,0 +1,7 @@
+export const CumulativeWithLimitDiscountStrategyName = Symbol.for(
+  'CumulativeWithLimitStrategy',
+);
+
+export const BiggestWithLimitDiscountStrategyName = Symbol.for(
+  'BiggestWithLimitStrategy',
+);
